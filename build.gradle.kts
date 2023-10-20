b6d90be05fe33545eb1a98d8fc86e8959f06f09f
Alexander Laptev <alexander_laptev4@inbox.ru>
2023-10-20 16:23:46 +0300

    build: switch from Maven to Gradle

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("org.openjfx.javafxplugin") version "0.1.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+    `java-library`
+    application
+}
+
+repositories {
+    mavenCentral()
+}
+
+javafx {
+    modules("javafx.controls", "javafx.fxml")
+}
+
+group = "com.cgvsu"
+version = "0.1.0"
+description = "TriangleRasterization"
+java.sourceCompatibility = JavaVersion.VERSION_18
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.javadoc {
+    options.encoding = "UTF-8"
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
